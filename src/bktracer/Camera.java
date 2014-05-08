2ecfd2bd80a161e466a827a6ee6acc42da955591
Brandon Kolze <brandon.kolze@gmail.com>
2014-05-07 23:05:08 -0400

    Added Camera and Color classes.

---
+++ src/bktracer/Camera.java
@@ -0,0 +1,12 @@
+package bktracer;
+
+/**
+ * Created by brandon on 5/7/14.
+ */
+public class Camera {
+    private Ray viewpoint;
+
+    public Camera(){
+    }
+
+}
---
+++ src/bktracer/Color.java
@@ -0,0 +1,29 @@
+package bktracer;
+
+/**
+ * Created by brandon on 5/7/14.
+ */
+public class Color {
+    private int r, g, b;
+
+    public int getR(){
+        return r;
+    }
+
+    public int getG(){
+        return g;
+    }
+
+    public int getB(){
+        return b;
+    }
+
+    public Color(){
+    }
+
+    public Color(int r, int g, int b){
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+}
